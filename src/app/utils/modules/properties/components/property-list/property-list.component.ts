dd54fe9cb1ead3610be3c085b647a0b81858699f
Glailson <glailsonmguedes@gmail.com>
2021-03-01 14:56:43 -0300

    List of Property

--- src/app/utils/modules/properties/components/property-list/property-list.component.ts
+++ src/app/utils/modules/properties/components/property-list/property-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import { ListComponentClass } from 'src/app/utils/classes/list-component.class';
 
 @Component({
@@ -6,4 +6,17 @@ import { ListComponentClass } from 'src/app/utils/classes/list-component.class';
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.sass']
 })
-export class PropertyListComponent extends ListComponentClass { }
+export class PropertyListComponent extends ListComponentClass {
+
+  @Output()
+  blockUnblock: EventEmitter<any> = new EventEmitter();
+
+  handleBlockUnblock(value: any): void {
+    value.preventDefault();
+    this.blockUnblock.emit({
+      targetId: value.target.value,
+      block: !value.target.checked,
+      reason: null
+    });
+  }
+}
--- src/app/utils/modules/properties/containers/properties-list/properties-list.component.ts
+++ src/app/utils/modules/properties/containers/properties-list/properties-list.component.ts
@@ -3,6 +3,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ListContainerClass } from 'src/app/utils/classes/list-container.class';
 import { FormDataModel } from 'src/app/utils/functions/generate-form-data.function';
 import { HttpService } from 'src/app/utils/services/http/http.service';
+import {ModalConfirmData} from "../../../shared/components/modal-confirm/modal-confirm.interface";
+import {ModalConfirmComponent} from "../../../shared/components/modal-confirm/modal-confirm.component";
+import {takeUntil} from "rxjs/operators";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {MegaleiosAlertService} from "../../../megaleios-alert/megaleios-alert.service";
 
 @Component({
   selector: 'app-properties-list',
@@ -13,7 +18,10 @@ export class PropertiesListComponent extends ListContainerClass {
 
   formDataModel: FormDataModel = {
     columns: [
-      { data: 'code', name: 'code', searchable: true }
+      { data: 'code', name: 'Code', searchable: true },
+      { data: 'residencialPropertyFeatures.typeProperty', name: 'TypeProperty', searchable: true },
+      { data: 'residencialPropertyFeatures.propertyValue', name: 'PropertyValue', searchable: true },
+      { data: 'residencialPropertyAdress.neighborhood', name: 'Neighborhood', searchable: true },
     ],
     page: 1,
     pageSize: 10,
@@ -26,18 +34,55 @@ export class PropertiesListComponent extends ListContainerClass {
     }
   };
 
-  uri = 'Property';
+  uri = 'ResidencialProperty';
 
   constructor(
     activatedRoute: ActivatedRoute,
     router: Router,
-    httpService: HttpService
+    httpService: HttpService,
+    private ngbModal: NgbModal,
+    private megaleiosAlertService: MegaleiosAlertService
   ) {
     super(activatedRoute, router, httpService);
   }
 
   handleDetails(): void {
     this._router.navigate(['/administrador/app/imoveis', this.listSelected[0].id]);
+  }
+
+  handleBlockUnblock(value: any): void {
+
+    let modalConfirmData: ModalConfirmData;
+    if (value.block) {
+      modalConfirmData = {
+        title: 'Inativar imóvel',
+        content: 'Deseja realmente inativar esse imóvel?',
+        action: 'inativar',
+      };
+    } else {
+      modalConfirmData = {
+        title: 'Ativar imóvel',
+        content: 'Deseja realmente ativar esse imóvel?',
+        action: 'ativar',
+      };
+    }
+    const modalRef = this.ngbModal.open(ModalConfirmComponent, { centered: true });
+    modalRef.componentInstance.modalConfirmData = modalConfirmData;
+    modalRef.result
+      .then((result: any) => {
+        if (result) {
+          this._httpService.post('ResidencialProperty/BlockUnblock', value)
+            .pipe(takeUntil(this.onDestroy))
+            .subscribe((response: any) => {
+              this.megaleiosAlertService.success(response.message);
+              const index = this.list.findIndex((item) => item.id === value.targetId);
+              this.list[index].blocked = value.block;
+            }, (response: any) => {
+              this.megaleiosAlertService.error(response.message);
+            });
+        }
+      })
+      .catch(() => { });
   }
 
 }
