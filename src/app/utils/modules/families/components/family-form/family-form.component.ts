270d13d7e174874d9f5ac2354ad45372b296b6c4
Glailson <glailsonmguedes@gmail.com>
2021-03-25 14:22:42 -0300

    Update in Family

--- src/app/utils/modules/families/components/family-form/family-form.component.ts
+++ src/app/utils/modules/families/components/family-form/family-form.component.ts
@@ -4,8 +4,6 @@ import {sortBy} from 'lodash';
 import {FormComponentClass} from 'src/app/utils/classes/form-component.class';
 import {isCpfValid, trimWhiteSpace} from 'src/app/utils/functions/validators.function';
 import {GENDER_LIST, SCHOOLING_LIST, DEGREE_OF_KINSHIP_LIST} from 'src/app/utils/interfaces/families.interface';
-import {dateToString} from "../../../../functions/date.function";
-
 
 @Component({
   selector: 'app-family-form',
@@ -20,7 +18,21 @@ export class FamilyFormComponent extends FormComponentClass implements OnInit {
   holderForm: FormGroup;
   spouseForm: FormGroup;
   financialForm: FormGroup;
-  priorizationForm: FormGroup;
+
+  workFront: FormGroup;
+  permanentDisabled: FormGroup;
+  elderlyOverEighty: FormGroup;
+  yearsInSextyAndSeventyNine: FormGroup;
+  womanServedByProtectiveMeasure: FormGroup;
+  femaleBreadWinner: FormGroup;
+  singleParent: FormGroup;
+  familyWithMoreThanFivePeople: FormGroup;
+  childUnderEighteen: FormGroup;
+  headOfHouseholdWithoutIncome: FormGroup;
+  benefitOfContinuedProvision: FormGroup;
+  familyPurse: FormGroup;
+  involuntaryCohabitation: FormGroup;
+  familyIncomeOfUpTwoMinimumWages: FormGroup;
 
   get membersForm(): FormArray {
     return this.form.controls.members as FormArray;
@@ -32,50 +44,63 @@ export class FamilyFormComponent extends FormComponentClass implements OnInit {
     super();
     // FORM FILHOS
     this.holderForm = this.formBuilder.group({
-      number: [null, Validators.compose([trimWhiteSpace, Validators.required])],
-      name: [null, Validators.compose([trimWhiteSpace, Validators.required])],
-      cpf: [null, Validators.compose([Validators.required, isCpfValid])],
-      birthday: [null, Validators.required],
-      genre: [null],
-      email: [null, Validators.compose([trimWhiteSpace, Validators.email])],
-      phone: [null, trimWhiteSpace],
+      number:     [null, Validators.compose([trimWhiteSpace, Validators.required])],
+      name:       [null, Validators.compose([trimWhiteSpace, Validators.required])],
+      cpf:        [null, Validators.compose([Validators.required, isCpfValid])],
+      birthday:   [null, Validators.required],
+      genre:      [null],
+      email:      [null, Validators.compose([trimWhiteSpace, Validators.email])],
+      phone:      [null, trimWhiteSpace],
       scholarity: [null, Validators.required],
     });
     this.spouseForm = this.formBuilder.group({
-      name: [null, trimWhiteSpace],
-      birthday: [null],
-      genre: [null],
+      name:       [null, trimWhiteSpace],
+      birthday:   [null],
+      genre:      [null],
       scholarity: [null],
     });
     this.financialForm = this.formBuilder.group({
-      familyIncome: [null, Validators.required],
+      familyIncome:               [null, Validators.required],
       propertyValueForDemolished: [null, Validators.required],
-      maximumPurchase: [null, Validators.required],
-      incrementValue: [null, Validators.required],
+      maximumPurchase:            [null, Validators.required],
+      incrementValue:             [null, Validators.required],
     });
-    this.priorizationForm = this.formBuilder.group({
-      workFront: [false],
-      permanentDisabled:  [false],
-      elderlyOverEighty:  [false],
-      yearsInSextyAndSeventyNine:  [false],
-      womanServedByProtectiveMeasure:  [false],
-      femaleBreadWinner:  [false],
-      singleParent:  [false],
-      familyWithMoreThanFivePeople:  [false],
-      childUnderEighteen:  [false],
-      headOfHouseholdWithoutIncome:  [false],
-      benefitOfContinuedProvision:  [false],
-      familyPurse:  [false],
-      involuntaryCohabitation:  [false],
-      familyIncomeOfUpTwoMinimumWages:  [false]
-    });
+    this.workFront =                        this.formBuilder.group({ rate: 1,  value: false });
+    this.permanentDisabled =                this.formBuilder.group({ rate: 2,  value: false });
+    this.elderlyOverEighty =                this.formBuilder.group({ rate: 3,  value: false });
+    this.yearsInSextyAndSeventyNine =       this.formBuilder.group({ rate: 14, value: false });
+    this.womanServedByProtectiveMeasure =   this.formBuilder.group({ rate: 4,  value: false });
+    this.femaleBreadWinner =                this.formBuilder.group({ rate: 5,  value: false });
+    this.singleParent =                     this.formBuilder.group({ rate: 6,  value: false });
+    this.familyWithMoreThanFivePeople =     this.formBuilder.group({ rate: 7,  value: false });
+    this.childUnderEighteen =               this.formBuilder.group({ rate: 8,  value: false });
+    this.headOfHouseholdWithoutIncome =     this.formBuilder.group({ rate: 9,  value: false });
+    this.benefitOfContinuedProvision =      this.formBuilder.group({ rate: 10, value: false });
+    this.familyPurse =                      this.formBuilder.group({ rate: 11, value: false });
+    this.involuntaryCohabitation =          this.formBuilder.group({ rate: 12, value: false });
+    this.familyIncomeOfUpTwoMinimumWages =  this.formBuilder.group({ rate: 13, value: false });
     // FORM PAI
     this.form = this.formBuilder.group({
-      holder: this.holderForm,
-      spouse: this.spouseForm,
-      members: this.formBuilder.array([]),
-      financial: this.financialForm,
-      priorization: this.priorizationForm,
+      holder:       this.holderForm,
+      spouse:       this.spouseForm,
+      members:      this.formBuilder.array([]),
+      financial:    this.financialForm,
+      priorization: [
+        this.workFront,
+        this.permanentDisabled,
+        this.elderlyOverEighty,
+        this.yearsInSextyAndSeventyNine,
+        this.womanServedByProtectiveMeasure,
+        this.femaleBreadWinner,
+        this.singleParent,
+        this.familyWithMoreThanFivePeople,
+        this.childUnderEighteen,
+        this.headOfHouseholdWithoutIncome,
+        this.benefitOfContinuedProvision,
+        this.familyPurse,
+        this.involuntaryCohabitation,
+        this.familyIncomeOfUpTwoMinimumWages,
+      ],
 
     });
   }
@@ -83,29 +108,27 @@ export class FamilyFormComponent extends FormComponentClass implements OnInit {
   ngOnInit(): void {
     if (this.formData) {
       this.form.patchValue(this.formData);
-      this.priorizationForm.patchValue({
-        workFront: `${this.formData.priorization.workFront}`,
-        permanentDisabled: `${this.formData.priorization.permanentDisabled}`,
-        elderlyOverEighty: `${this.formData.priorization.elderlyOverEighty}`,
-        yearsInSextyAndSeventyNine: `${this.formData.priorization.yearsInSextyAndSeventyNine}`,
-        womanServedByProtectiveMeasure: `${this.formData.priorization.womanServedByProtectiveMeasure}`,
-        femaleBreadWinner: `${this.formData.priorization.femaleBreadWinner}`,
-        singleParent: `${this.formData.priorization.singleParent}`,
-        familyWithMoreThanFivePeople: `${this.formData.priorization.familyWithMoreThanFivePeople}`,
-        childUnderEighteen: `${this.formData.priorization.childUnderEighteen}`,
-        headOfHouseholdWithoutIncome: `${this.formData.priorization.headOfHouseholdWithoutIncome}`,
-        benefitOfContinuedProvision: `${this.formData.priorization.benefitOfContinuedProvision}`,
-        familyPurse: `${this.formData.priorization.familyPurse}`,
-        involuntaryCohabitation: `${this.formData.priorization.involuntaryCohabitation}`,
-        familyIncomeOfUpTwoMinimumWages: `${this.formData.priorization.familyIncomeOfUpTwoMinimumWages}`
-      });
+      this.workFront.patchValue({rate: 1, value:  `${this.formData.priorization.workFront.value}`});
+      this.permanentDisabled.patchValue({rate: 2, value:  `${this.formData.priorization.permanentDisabled.value}`});
+      this.elderlyOverEighty.patchValue({rate: 3, value:  `${this.formData.priorization.elderlyOverEighty.value}`});
+      this.yearsInSextyAndSeventyNine.patchValue({rate: 14, value:  `${this.formData.priorization.yearsInSextyAndSeventyNine.value}`});
+      this.womanServedByProtectiveMeasure.patchValue({rate: 4, value:  `${this.formData.priorization.womanServedByProtectiveMeasure.value}`});
+      this.femaleBreadWinner.patchValue({rate: 5, value:  `${this.formData.priorization.femaleBreadWinner.value}`});
+      this.singleParent.patchValue({rate: 6, value:  `${this.formData.priorization.singleParent.value}`});
+      this.familyWithMoreThanFivePeople.patchValue({rate: 7, value:  `${this.formData.priorization.familyWithMoreThanFivePeople.value}`});
+      this.childUnderEighteen.patchValue({rate: 8, value:  `${this.formData.priorization.childUnderEighteen.value}`});
+      this.headOfHouseholdWithoutIncome.patchValue({rate: 9, value:  `${this.formData.priorization.headOfHouseholdWithoutIncome.value}`});
+      this.benefitOfContinuedProvision.patchValue({rate: 10, value:  `${this.formData.priorization.benefitOfContinuedProvision.value}`});
+      this.familyPurse.patchValue({rate: 11, value:  `${this.formData.priorization.familyPurse.value}`});
+      this.involuntaryCohabitation.patchValue({rate: 12, value:  `${this.formData.priorization.involuntaryCohabitation.value}`});
+      this.familyIncomeOfUpTwoMinimumWages.patchValue({rate: 13, value:  `${this.formData.priorization.familyIncomeOfUpTwoMinimumWages.value}`});
       for (let i = 0; this.formData.members.length > i; i++) {
         this.membersForm.push(
           this.formBuilder.group({
-            name: [this.formData.members[i].name, Validators.compose([trimWhiteSpace, Validators.required])],
-            birthday: [this.formData.members[i].birthday, Validators.required],
-            genre: [this.formData.members[i].genre, Validators.required],
-            kinShip: [this.formData.members[i].kinShip, Validators.required],
+            name:       [this.formData.members[i].name, Validators.compose([trimWhiteSpace, Validators.required])],
+            birthday:   [this.formData.members[i].birthday, Validators.required],
+            genre:      [this.formData.members[i].genre, Validators.required],
+            kinShip:    [this.formData.members[i].kinShip, Validators.required],
             scholarity: [this.formData.members[i].scholarity, Validators.required]
           })
         );
@@ -115,10 +138,10 @@ export class FamilyFormComponent extends FormComponentClass implements OnInit {
 
   private createMemberForm(): FormGroup {
     return this.formBuilder.group({
-      name: [null, Validators.compose([trimWhiteSpace, Validators.required])],
-      birthday: [null, Validators.required],
-      genre: [null, Validators.required],
-      kinShip: [null, Validators.required],
+      name:       [null, Validators.compose([trimWhiteSpace, Validators.required])],
+      birthday:   [null, Validators.required],
+      genre:      [null, Validators.required],
+      kinShip:    [null, Validators.required],
       scholarity: [null, Validators.required]
     });
   }
