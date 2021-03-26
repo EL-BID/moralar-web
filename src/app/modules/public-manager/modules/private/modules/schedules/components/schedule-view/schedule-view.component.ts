acdb220bd3b8ac04d0ccbac042caaf034535401f
Glailson <glailsonmguedes@gmail.com>
2021-03-26 19:59:57 -0300

    schedule - choose property

--- src/app/modules/public-manager/modules/private/modules/schedules/components/schedule-view/schedule-view.component.ts
+++ src/app/modules/public-manager/modules/private/modules/schedules/components/schedule-view/schedule-view.component.ts
@@ -2,6 +2,11 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {SCHEDULE_STATUS_LIST, SCHEDULE_TYPE_LIST} from '../../../../../../../../utils/interfaces/schedules.interface';
 import { sortBy } from 'lodash';
 import {dateAndTimeToString} from '../../../../../../../../utils/functions/date.function';
+import {ModalConfirmData} from '../../../../../../../../utils/modules/shared/components/modal-confirm/modal-confirm.interface';
+import {ModalConfirmComponent} from '../../../../../../../../utils/modules/shared/components/modal-confirm/modal-confirm.component';
+import {HttpService} from '../../../../../../../../utils/services/http/http.service';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {MegaleiosAlertService} from '../../../../../../../../utils/modules/megaleios-alert/megaleios-alert.service';
 @Component({
   selector: 'app-schedule-view',
   templateUrl: './schedule-view.component.html',
@@ -18,7 +23,11 @@ export class ScheduleViewComponent implements OnInit{
   @Output()
   reschedule: EventEmitter<void> = new EventEmitter();
 
-  constructor() {
+  constructor(
+    private httpService: HttpService,
+    private ngbModal: NgbModal,
+    private megaleiosAlertService: MegaleiosAlertService
+  ) {
 
   }
 
@@ -26,4 +35,30 @@ export class ScheduleViewComponent implements OnInit{
     this.data = dateAndTimeToString(+this.schedule.date);
   }
 
+  chooseProperty(value: any): void {
+    value.typeSubject = 4;
+    let modalConfirmData: ModalConfirmData;
+
+    modalConfirmData = {
+      title: 'Escolha do imóvel',
+      content: 'Deseja realmente liberar para escolha do imóvel?',
+      action: 'liberar',
+    };
+
+    const modalRef = this.ngbModal.open(ModalConfirmComponent, { centered: true });
+    modalRef.componentInstance.modalConfirmData = modalConfirmData;
+    modalRef.result
+      .then((result: any) => {
+        if (result) {
+          this.httpService.post('Schedule/ChangeSubject', value)
+            .subscribe((response: any) => {
+              this.megaleiosAlertService.success(response.message);
+              location.reload();
+            }, (response: any) => {
+              this.megaleiosAlertService.error(response.message);
+            });
+        }
+      })
+      .catch(() => { });
+  }
 }
