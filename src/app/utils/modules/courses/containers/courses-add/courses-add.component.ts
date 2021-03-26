fbeec7747be02251202f4d6885bfcdb81cea373b
Glailson <glailsonmguedes@gmail.com>
2021-03-26 15:34:06 -0300

    Course - return after inclusion

--- src/app/utils/modules/courses/containers/courses-add/courses-add.component.ts
+++ src/app/utils/modules/courses/containers/courses-add/courses-add.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import { takeUntil } from 'rxjs/operators';
 import { OnDestroyClass } from 'src/app/utils/classes/on-destroy.class';
 import { MegaleiosAlertService } from 'src/app/utils/modules/megaleios-alert/megaleios-alert.service';
 import { HttpService } from 'src/app/utils/services/http/http.service';
-import {dateToSeconds} from "../../../../functions/date.function";
+import {dateToSeconds} from '../../../../functions/date.function';
 
 @Component({
   selector: 'app-courses-add',
@@ -16,6 +16,7 @@ export class CoursesAddComponent extends OnDestroyClass {
   formLoading = false;
 
   constructor(
+    private activatedRoute: ActivatedRoute,
     private httpService: HttpService,
     private megaleiosAlertService: MegaleiosAlertService,
     private router: Router
@@ -32,7 +33,7 @@ export class CoursesAddComponent extends OnDestroyClass {
         .pipe(takeUntil(this.onDestroy))
         .subscribe((response: any) => {
           this.megaleiosAlertService.success(response.message);
-          this.router.navigate(['Course/LoadData']);
+          this.router.navigate(['../'], { relativeTo: this.activatedRoute });
         }, (response: any) => {
           this.megaleiosAlertService.error(response.message);
           this.formLoading = false;
